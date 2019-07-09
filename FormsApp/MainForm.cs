f3a174b4567d9fc87014c63a6d848241f1d2d700
554393109 <554393109@qq.com>
2019-07-09 18:15:03 +0800

    Modify

--- FormsApp/MainForm.cs
+++ FormsApp/MainForm.cs
@@ -8,18 +8,46 @@ using System.IO;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Windows.Forms;
+using static FormsApp.Easy;
 
 namespace FormsApp
 {
     public partial class MainForm : Form
     {
+        private static CURLcode sm_curlCode;
+        //public delegate int ReadFunction(byte[] buf, int size, int nmemb, Object extraData);
+        public delegate int ReadFunction(IntPtr buf, int size, int nmemb, IntPtr extraData);
+        public static ReadFunction m_pfRead;
+        public Object m_readData;
+
         public MainForm()
         {
             InitializeComponent();
-        }
 
+            sm_curlCode = Curl.GlobalInit((int)CURLinitFlag.CURL_GLOBAL_ALL);
+        }
+        
         private void btn_Post_Click(object sender, EventArgs e)
         {
+            //IntPtr m_pCURL = External.curl_easy_init();
+
+            //CURLcode retCode = CURLcode.CURLE_OK;
+            //retCode = External.curl_easy_setopt(m_pCURL, CURLoption.CURLOPT_URL, Marshal.StringToCoTaskMemAnsi("https://account.storepos.cn/UnifiedPay/Mch_Reg"));
+            //retCode = External.curl_easy_setopt(m_pCURL, CURLoption.CURLOPT_POSTFIELDS, Marshal.StringToCoTaskMemAnsi($"a={Uri.EscapeDataString("我是中文")}&b={Uri.EscapeDataString("i am English")}"));
+            //retCode = External.curl_easy_setopt(m_pCURL, CURLoption.CURLOPT_USERAGENT, Marshal.StringToCoTaskMemAnsi("我是尹自强的工具"));
+            //retCode = External.curl_easy_setopt(m_pCURL, CURLoption.CURLOPT_SSL_VERIFYHOST, (IntPtr)0);
+            //retCode = External.curl_easy_setopt(m_pCURL, CURLoption.CURLOPT_SSL_VERIFYPEER, (IntPtr)0);
+
+            //CURLcode res = External.curl_easy_perform(m_pCURL);
+            //if (res != CURLcode.CURLE_OK)
+            //{
+            //    // Error
+            //}
+
+            //External.curl_easy_cleanup(m_pCURL);
+
+            //return;
+
             try
             {
                 using (var pro = new Process())
